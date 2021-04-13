97f27f18bc2b4d6b94e663b2f436015abbe6846f
Tonnie Ishangu <67873141+tonyishangu@users.noreply.github.com>
2021-04-13 09:17:29 +0300

    add projectid username and password

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { ChatEngine } from 'react-chat-engine';
 function App() {
   return (
     <ChatEngine
-    
+      height="100vh"
+      projectID="e8235634-5996-431c-839d-11325df1c3aa"
+      userName="Tonnie"
+      userSecret="Saige1aw"
     />
   );
 }
