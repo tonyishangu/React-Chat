8063f7f85713e07e9d5397389fa619932bcec604
Tonnie Ishangu <67873141+tonyishangu@users.noreply.github.com>
2021-04-13 09:06:18 +0300

    add chatengine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ChatEngine
+    
+    />
   );
 }
 
